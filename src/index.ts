f8a5d533cdf63a17ef2641ee7ade7d69ecf34e2e
Jack Herrington <jherr@pobox.com>
2025-02-14 15:57:15 -0800

    Changing to direct imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { Command, InvalidArgumentError } from "commander";
-import * as fs from "node:fs/promises";
-import * as path from "node:path";
+import { mkdir, copyFile, readFile, writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { intro, outro, spinner } from "@clack/prompts";
 import { execa } from "execa";
@@ -25,14 +25,14 @@ async function createApp(projectName: string, options: Required<Options>) {
   const templateDir = fileURLToPath(
     new URL("../project-template", import.meta.url)
   );
-  const targetDir = path.join(process.cwd(), projectName);
+  const targetDir = resolve(process.cwd(), projectName);
 
   async function copyFiles(files: string[]) {
     for (const file of files) {
       const targetFileName = file.replace(".tw", "");
-      await fs.copyFile(
-        path.join(templateDir, file),
-        path.join(targetDir, targetFileName)
+      await copyFile(
+        resolve(templateDir, file),
+        resolve(targetDir, targetFileName)
       );
     }
   }
@@ -40,17 +40,17 @@ async function createApp(projectName: string, options: Required<Options>) {
   intro(`Creating a new TanStack app in ${targetDir}...`);
 
   // Make the root directory
-  await fs.mkdir(targetDir, { recursive: true });
+  await mkdir(targetDir, { recursive: true });
 
   // Setup the .vscode directory
-  await fs.mkdir(path.join(targetDir, ".vscode"), { recursive: true });
-  await fs.copyFile(
-    path.join(templateDir, ".vscode/settings.json"),
-    path.join(targetDir, ".vscode/settings.json")
+  await mkdir(resolve(targetDir, ".vscode"), { recursive: true });
+  await copyFile(
+    resolve(templateDir, ".vscode/settings.json"),
+    resolve(targetDir, ".vscode/settings.json")
   );
 
   // Fill the public directory
-  await fs.mkdir(path.join(targetDir, "public"), { recursive: true });
+  await mkdir(resolve(targetDir, "public"), { recursive: true });
   copyFiles([
     "./public/robots.txt",
     "./public/favicon.ico",
@@ -60,7 +60,7 @@ async function createApp(projectName: string, options: Required<Options>) {
   ]);
 
   // Make the src directory
-  await fs.mkdir(path.join(targetDir, "src"), { recursive: true });
+  await mkdir(resolve(targetDir, "src"), { recursive: true });
 
   // Copy in Vite and Tailwind config and CSS
   if (options.tailwind) {
@@ -73,8 +73,8 @@ async function createApp(projectName: string, options: Required<Options>) {
 
   // Setup the app component. There are four variations, typescript/javascript and tailwind/non-tailwind.
   let appComponent = (
-    await fs.readFile(
-      path.join(
+    await readFile(
+      resolve(
         templateDir,
         options.tailwind ? "./src/App.tw.tsx" : "./src/App.tsx"
       ),
@@ -84,17 +84,17 @@ async function createApp(projectName: string, options: Required<Options>) {
   if (!options.typescript) {
     appComponent = appComponent.replace("App.tsx", "App.jsx");
   }
-  await fs.writeFile(
-    path.join(targetDir, `./src/App${options.typescript ? ".tsx" : ".jsx"}`),
+  await writeFile(
+    resolve(targetDir, `./src/App${options.typescript ? ".tsx" : ".jsx"}`),
     appComponent
   );
 
   // Setup the main, reportWebVitals and index.html files
   if (options.typescript) {
     await copyFiles(["./src/main.tsx", "./src/reportWebVitals.ts"]);
-    await fs.copyFile(
-      path.join(templateDir, `./index.ts.html`),
-      path.join(targetDir, "./index.html")
+    await copyFile(
+      resolve(templateDir, `./index.ts.html`),
+      resolve(targetDir, "./index.html")
     );
   } else {
     await copyFiles([
@@ -111,12 +111,12 @@ async function createApp(projectName: string, options: Required<Options>) {
 
   // Setup the package.json file, optionally with typescript and tailwind
   let packageJSON = JSON.parse(
-    await fs.readFile(path.join(templateDir, "package.json"), "utf8")
+    await readFile(resolve(templateDir, "package.json"), "utf8")
   );
   packageJSON.name = projectName;
   if (options.typescript) {
     const tsPackageJSON = JSON.parse(
-      await fs.readFile(path.join(templateDir, "package.ts.json"), "utf8")
+      await readFile(resolve(templateDir, "package.ts.json"), "utf8")
     );
     packageJSON = {
       ...packageJSON,
@@ -128,7 +128,7 @@ async function createApp(projectName: string, options: Required<Options>) {
   }
   if (options.tailwind) {
     const twPackageJSON = JSON.parse(
-      await fs.readFile(path.join(templateDir, "package.tw.json"), "utf8")
+      await readFile(resolve(templateDir, "package.tw.json"), "utf8")
     );
     packageJSON = {
       ...packageJSON,
@@ -138,19 +138,19 @@ async function createApp(projectName: string, options: Required<Options>) {
       },
     };
   }
-  await fs.writeFile(
-    path.join(targetDir, "package.json"),
+  await writeFile(
+    resolve(targetDir, "package.json"),
     JSON.stringify(packageJSON, null, 2)
   );
 
   // Add .gitignore and README.md
-  await fs.copyFile(
-    path.join(templateDir, "gitignore"),
-    path.join(targetDir, ".gitignore")
+  await copyFile(
+    resolve(templateDir, "gitignore"),
+    resolve(targetDir, ".gitignore")
   );
-  await fs.copyFile(
-    path.join(templateDir, "README.md"),
-    path.join(targetDir, "README.md")
+  await copyFile(
+    resolve(templateDir, "README.md"),
+    resolve(targetDir, "README.md")
   );
 
   // Install dependencies
