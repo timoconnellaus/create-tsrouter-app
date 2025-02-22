7f95ac7f9e81d90ee60df2bc389a17c210f6c9cc
Jack Herrington <jherr@pobox.com>
2025-02-22 09:34:22 -0800

    feat: now Solid has add-ons

---
+++ templates/solid/add-on/store/assets/src/routes/demo.store.page2.tsx
@@ -0,0 +1,30 @@
+import { createFileRoute } from '@tanstack/solid-router'
+import { useStore } from '@tanstack/solid-store'
+
+import { store } from '../lib/demo-store'
+
+export const Route = createFileRoute('/demo/store/page2')({
+  component: App,
+})
+
+function App() {
+  const count = useStore(store, (state) => state.count)
+
+  return (
+    <div class="p-4 flex flex-col gap-2">
+      <p class="text-2xl">Global Count: {count}</p>
+      <button
+        onClick={() => {
+          store.setState((state) => ({
+            count: state.count + 10,
+          }))
+        }}
+        class="mt-4 inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Increment count by 10
+      </button>
+    </div>
+  )
+}
+
+export default App
