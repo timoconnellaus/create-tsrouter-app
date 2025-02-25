728ce62e7630d36fe6369022fc3034c59864ce62
Jack Herrington <jherr@pobox.com>
2025-02-24 20:31:24 -0800

    feat: better Store example

--- templates/solid/add-on/store/assets/src/lib/demo-store.ts
+++ templates/solid/add-on/store/assets/src/lib/demo-store.ts
@@ -1,5 +1,13 @@
-import { Store } from '@tanstack/store'
+import { Derived, Store } from '@tanstack/store'
 
 export const store = new Store({
-  count: 0,
+  firstName: 'Jane',
+  lastName: 'Smith',
 })
+
+export const fullName = new Derived({
+  fn: () => `${store.state.firstName} ${store.state.lastName}`,
+  deps: [store],
+})
+
+fullName.mount()
