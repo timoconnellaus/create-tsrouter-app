1bb5f90d31d12b8d81e51317ba7e3098c9e08314
Jack Herrington <jherr@pobox.com>
2025-03-10 12:03:42 -0700

    fix: minor change to bump version and deploy

--- src/create-app.ts
+++ src/create-app.ts
@@ -712,7 +712,7 @@ ${environment.getErrors().join('\n')}`
       startCommand = `deno ${isAddOnEnabled('start') ? 'task dev' : 'start'}`
     }
 
-    outro(`Created your new TanStack app in '${basename(targetDir)}'.
+    outro(`Created your TanStack app in '${basename(targetDir)}'.
 
 Use the following commands to start your app:
 % cd ${options.projectName}
