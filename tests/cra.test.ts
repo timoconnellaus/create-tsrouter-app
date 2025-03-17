f1400923947a7882136fa6d67ae7877b62c29f2d
Ilya Shaplyko <shaplyko.ilya@gmail.com>
2025-03-17 16:32:29 +0100

    Fix broken link and add solid snapshots for cra test (#61)
    Sorry I didn't update all the files and snapshots in previous MR.
    
    Fixed final broken link. 
    
    Realized there is no solid snapshot tests so I added them similar to
    current react snapshots. I hope thats fine, let me know what you think.

--- tests/cra.test.ts
+++ tests/cra.test.ts
@@ -1,116 +1,232 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
 import { createApp } from '../src/create-app.js'
 import { cleanupOutput, createTestEnvironment } from './test-utilities.js'
 
-test('code router in javascript on npm', async () => {
-  const projectName = 'TEST'
-  const { environment, output } = createTestEnvironment(projectName)
-  await createApp(
-    {
-      addOns: false,
-      framework: 'react',
-      chosenAddOns: [],
-      git: true,
-      mode: 'code-router',
-      packageManager: 'npm',
-      projectName,
-      tailwind: false,
-      toolchain: 'none',
-      typescript: false,
-      variableValues: {},
-    },
-    {
-      silent: true,
-      environment,
-    },
-  )
-  cleanupOutput(output)
-  await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
-    './snapshots/cra/cr-js-npm.json',
-  )
+describe('React Templates', () => {
+  test('code router in javascript on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'react',
+        chosenAddOns: [],
+        git: true,
+        mode: 'code-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: false,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/cr-js-npm.json',
+    )
+  })
+
+  test('code router in typescript on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'react',
+        chosenAddOns: [],
+        git: true,
+        mode: 'code-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/cr-ts-npm.json',
+    )
+  })
+
+  test('file router on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'react',
+        chosenAddOns: [],
+        git: true,
+        mode: 'file-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/fr-ts-npm.json',
+    )
+  })
+
+  test('file router with tailwind on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'react',
+        chosenAddOns: [],
+        git: true,
+        mode: 'file-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: true,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/fr-ts-tw-npm.json',
+    )
+  })
 })
 
-test('code router in typescript on npm', async () => {
-  const projectName = 'TEST'
-  const { environment, output } = createTestEnvironment(projectName)
-  await createApp(
-    {
-      addOns: false,
-      framework: 'react',
-      chosenAddOns: [],
-      git: true,
-      mode: 'code-router',
-      packageManager: 'npm',
-      projectName,
-      tailwind: false,
-      toolchain: 'none',
-      typescript: true,
-      variableValues: {},
-    },
-    {
-      silent: true,
-      environment,
-    },
-  )
-  cleanupOutput(output)
-  await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
-    './snapshots/cra/cr-ts-npm.json',
-  )
-})
+describe('Solid Templates', () => {
+  test('code router in javascript on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'solid',
+        chosenAddOns: [],
+        git: true,
+        mode: 'code-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: false,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/solid-cr-js-npm.json',
+    )
+  })
 
-test('file router on npm', async () => {
-  const projectName = 'TEST'
-  const { environment, output } = createTestEnvironment(projectName)
-  await createApp(
-    {
-      addOns: false,
-      framework: 'react',
-      chosenAddOns: [],
-      git: true,
-      mode: 'file-router',
-      packageManager: 'npm',
-      projectName,
-      tailwind: false,
-      toolchain: 'none',
-      typescript: true,
-      variableValues: {},
-    },
-    {
-      silent: true,
-      environment,
-    },
-  )
-  cleanupOutput(output)
-  await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
-    './snapshots/cra/fr-ts-npm.json',
-  )
-})
+  test('code router in typescript on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'solid',
+        chosenAddOns: [],
+        git: true,
+        mode: 'code-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/solid-cr-ts-npm.json',
+    )
+  })
 
-test('file router with tailwind on npm', async () => {
-  const projectName = 'TEST'
-  const { environment, output } = createTestEnvironment(projectName)
-  await createApp(
-    {
-      addOns: false,
-      framework: 'react',
-      chosenAddOns: [],
-      git: true,
-      mode: 'file-router',
-      packageManager: 'npm',
-      projectName,
-      tailwind: true,
-      toolchain: 'none',
-      typescript: true,
-      variableValues: {},
-    },
-    {
-      silent: true,
-      environment,
-    },
-  )
-  cleanupOutput(output)
-  await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
-    './snapshots/cra/fr-ts-tw-npm.json',
-  )
+  test('file router on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'solid',
+        chosenAddOns: [],
+        git: true,
+        mode: 'file-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: false,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/solid-fr-ts-npm.json',
+    )
+  })
+
+  test('file router with tailwind on npm', async () => {
+    const projectName = 'TEST'
+    const { environment, output } = createTestEnvironment(projectName)
+    await createApp(
+      {
+        addOns: false,
+        framework: 'solid',
+        chosenAddOns: [],
+        git: true,
+        mode: 'file-router',
+        packageManager: 'npm',
+        projectName,
+        tailwind: true,
+        toolchain: 'none',
+        typescript: true,
+        variableValues: {},
+      },
+      {
+        silent: true,
+        environment,
+      },
+    )
+    cleanupOutput(output)
+    await expect(JSON.stringify(output, null, 2)).toMatchFileSnapshot(
+      './snapshots/cra/solid-fr-ts-tw-npm.json',
+    )
+  })
 })
