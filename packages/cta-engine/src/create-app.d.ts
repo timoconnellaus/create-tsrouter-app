286a01fbeca232c80da1a4436498b0bffb8102b8
Jack Herrington <jherr@pobox.com>
2025-04-13 09:27:33 -0700

    chore: removing bad files

--- packages/cta-engine/src/create-app.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Environment, Options } from '@tanstack/cta-core';
-export declare function createApp(options: Options, { silent, environment, cwd, appName, }: {
-    silent?: boolean;
-    environment: Environment;
-    cwd?: string;
-    name?: string;
-    appName?: string;
-}): Promise<void>;
