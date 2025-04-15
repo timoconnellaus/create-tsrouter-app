2a2c6d5304d529c9e516c032f906e22a56019981
Jack Herrington <jherr@pobox.com>
2025-04-15 09:29:02 -0700

    chore: fixes for solid

--- templates/solid/add-ons/tanstack-query/assets/src/routes/demo.tanstack-query.tsx
+++ templates/solid/add-ons/tanstack-query/assets/src/routes/demo.tanstack-query.tsx
@@ -9,20 +9,14 @@ function App() {
   const peopleQuery = createQuery(() => ({
     queryKey: ['people'],
     queryFn: () =>
-      fetch('https://swapi.dev/api/people')
-        .then((res) => res.json())
-        .then((data) => data.results as Array<{ name: string }>),
+      Promise.resolve([{ name: 'John Doe' }, { name: 'Jane Doe' }]),
     initialData: [],
   }))
 
   return (
     <div class="p-4">
       <h1 class="text-2xl mb-4">People list from Swapi</h1>
-      <ul>
-        {peopleQuery.data.map((person) => (
-          <li>{person.name}</li>
-        ))}
-      </ul>
+      <ul>{peopleQuery.data?.map((person) => <li>{person.name}</li>)}</ul>
     </div>
   )
 }
