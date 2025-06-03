ca45c0077bdeba6149eeae01750d5e8ee3ddd5f4
Brenley Dueck <brenleydueck@gmail.com>
2025-06-03 18:29:25 -0500

    chore: update solid-start add-on to alpha (#112)
    Ran this command to verify:
    
    `node ../create-tsrouter-app/cli/create-tsrouter-app/dist/index.js
    --template file-router --framework solid --add-ons start
    solid-start-test`
    
    A few things maybe not 100% but could be because of alpha.
    
    - FOUC seems to happen on first load
    
    Co-authored-by: Brenley Dueck <brenleydueck@Brenleys-MacBook-Pro.local>

--- frameworks/solid/add-ons/start/assets/src/routes/demo.start.server-funcs.tsx
+++ frameworks/solid/add-ons/start/assets/src/routes/demo.start.server-funcs.tsx
@@ -40,7 +40,7 @@ function Home() {
             router.invalidate()
           })
         }}
-        claclassssName="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Add 1 to {state}?
       </button>
