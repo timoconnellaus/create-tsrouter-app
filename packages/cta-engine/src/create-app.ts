c2e2f959cd14dc3cbfcce95987953a3ff6483cc4
Jack Herrington <jherr@pobox.com>
2025-07-01 07:09:07 -0700

    fix: updating CSS and package.json

--- packages/cta-engine/src/create-app.ts
+++ packages/cta-engine/src/create-app.ts
@@ -224,6 +224,7 @@ Errors were encountered during the creation of your app:
 ${environment.getErrors().join('\n')}`
   }
 
+  // Use the force luke! :)
   environment.outro(
     `Your ${environment.appName} app is ready in '${basename(options.targetDir)}'.
 
