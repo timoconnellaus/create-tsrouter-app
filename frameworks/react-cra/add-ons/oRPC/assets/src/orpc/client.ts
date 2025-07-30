718ad4c28f9c91734bd236cc2c45efffd232b81e
Jack Herrington <jherr@pobox.com>
2025-07-29 18:09:37 -0700

    fix: adding oRPC, upgrading tRPC

---
+++ frameworks/react-cra/add-ons/oRPC/assets/src/orpc/client.ts
@@ -0,0 +1,28 @@
+import { createRouterClient } from '@orpc/server'
+import type { RouterClient } from '@orpc/server'
+import { createORPCClient } from '@orpc/client'
+import { RPCLink } from '@orpc/client/fetch'
+import { createTanstackQueryUtils } from '@orpc/tanstack-query'
+import { getHeaders } from '@tanstack/react-start/server'
+import { createIsomorphicFn } from '@tanstack/react-start'
+
+import router from '@/orpc/router'
+
+const getORPCClient = createIsomorphicFn()
+  .server(() =>
+    createRouterClient(router, {
+      context: async () => ({
+        headers: getHeaders(),
+      }),
+    }),
+  )
+  .client((): RouterClient<typeof router> => {
+    const link = new RPCLink({
+      url: `${window.location.origin}/api/rpc`,
+    })
+    return createORPCClient(link)
+  })
+
+export const client: RouterClient<typeof router> = getORPCClient()
+
+export const orpc = createTanstackQueryUtils(client)
