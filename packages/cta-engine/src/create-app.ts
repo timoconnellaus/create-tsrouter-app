3bab3ff62c3b115fc4371e6c687f40bdc4a6dc29
Jack Herrington <jherr@pobox.com>
2025-08-06 17:41:48 -0700

    fix: disabling verbatimModuleSyntax on start

--- packages/cta-engine/src/create-app.ts
+++ packages/cta-engine/src/create-app.ts
@@ -234,7 +234,7 @@ Use the following commands to start your app:
       getPackageManagerScriptCommand(options.packageManager, ['dev']),
     )}
 
-Please check the README.md for information on testing, styling, adding routes, etc.${errorStatement}`,
+Please read the README.md for information on testing, styling, adding routes, etc.${errorStatement}`,
   )
 }
 
